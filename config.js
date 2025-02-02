8958fbfdf989011e279c845cbfd6ced1082c4b59
Aislx Flames <120901302+aislxflames@users.noreply.github.com>
2025-02-02 13:37:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ module.exports = {
   },
   INTERACTIONS: {
     SLASH: true, // Should the interactions be enabled
-    CONTEXT: false, // Should contexts be enabled
-    GLOBAL: true, // Should the interactions be registered globally
-    TEST_GUILD_ID: "1329786469618810961", // Guild ID where the interactions should be registered. [** Test you commands here first **]
+    CONTEXT: true, // Should contexts be enabled
+    GLOBAL: false, // Should the interactions be registered globally
+    TEST_GUILD_ID: "xxxxxxx", // Guild ID where the interactions should be registered. [** Test you commands here first **]
   },
   EMBED_COLORS: {
     BOT_EMBED: "#068ADD",
